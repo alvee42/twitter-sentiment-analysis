1995f30d2eb90730c409bd4659a651bf9e174109
Alvee Hoque <58802524+alvee42@users.noreply.github.com>
2021-06-22 22:46:53 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+### Keys: Ideally Import these tokens and keys from a config file 
+consumer_key = ''
+consumer_secret = ''
+
+access_token = ''
+access_token_secret = ''
